0cfd2495932a59ec052f881c4be4f2c8f2fc7143
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-06-20 21:37:49 +0700

    lokasi

--- system/lokasi.php
+++ system/lokasi.php
@@ -1,8 +1,8 @@
 
 <?php
 require_once('../connect.php');
-$latitude		= $_POST['latitude'];
-$longitude		= $_POST['longitude'];
+$latitude      = $_POST['latitude'];
+$longitude      = $_POST['longitude'];
 $id            = $_POST['id'];
 
 $stmt = $con->prepare('SELECT id_sopir FROM lokasi WHERE id_sopir = ?');
@@ -14,10 +14,12 @@ if ($stmt->num_rows > 0) {
    $stmt = $con->prepare('UPDATE lokasi SET latitude=?, longitude=? WHERE id_sopir=?');
    $stmt->bind_param('ssi', $latitude, $longitude, $id);
    $stmt->execute();
+   echo json_encode(['success' => true]);
 } else {
    $stmt = $con->prepare('INSERT INTO lokasi (id_sopir, latitude, longitude) VALUES (?, ?, ?)');
    $stmt->bind_param('iss', $id, $latitude, $longitude);
    $stmt->execute();
+   echo json_encode(['success' => true]);
 }
 
 ?>
--- system/tracking.php
+++ system/tracking.php
@@ -1,30 +1,30 @@
 <?php
-	require_once('header.php');
-	// require_once('../connect.php');
-	if ($_SESSION['level'] != 'sopir'){
-		echo '<script>			
+require_once('header.php');
+// require_once('../connect.php'); 
+if ($_SESSION['level'] != 'sopir') {
+	echo '<script>			
 				window.location.href = "index.php";
 			</script>';
-	}
+}
 
-	$id_sopir		= $_SESSION['id'];
+$id_sopir		= $_SESSION['id'];
 
-	if (isset($_POST['simpan'])){
-		$id_mobil		= $_POST['id_mobil'];
-		$mob 				= 0;
-		
-		$stmt = $con->prepare('UPDATE lokasi SET id_mobil=? WHERE id_mobil=?');
-		$stmt->bind_param('ii', $mob, $id_mobil);
+if (isset($_POST['simpan'])) {
+	$id_mobil		= $_POST['id_mobil'];
+	$mob 				= 0;
+
+	$stmt = $con->prepare('UPDATE lokasi SET id_mobil=? WHERE id_mobil=?');
+	$stmt->bind_param('ii', $mob, $id_mobil);
+	$stmt->execute();
+
+	if ($stmt = $con->prepare('UPDATE lokasi SET id_mobil=? WHERE id_sopir=?')) {
+		$stmt->bind_param('ii', $id_mobil, $id_sopir);
 		$stmt->execute();
-
-		if ($stmt = $con->prepare('UPDATE lokasi SET id_mobil=? WHERE id_sopir=?')) {
-			$stmt->bind_param('ii', $id_mobil, $id_sopir);
-			$stmt->execute();
-			$message = "Berhasil menyimpan data!";
-			echo '<script type="text/javascript">							
+		$message = "Berhasil menyimpan data!";
+		echo '<script type="text/javascript">							
 						Swal.fire({
 							title: "Sukses!",
-							text: "'.$message.'",
+							text: "' . $message . '",
 							type: "success",
 							timer: 2000,
 							showConfirmButton: false
@@ -32,14 +32,14 @@
 							window.location.href = "tracking.php";
 						});
 					</script>';
-			return false;
-		} else {
-			// Something is wrong with the sql statement.
-			$message = "Gagal menyimpan data!";
-			echo '<script type="text/javascript">							
+		return false;
+	} else {
+		// Something is wrong with the sql statement.
+		$message = "Gagal menyimpan data!";
+		echo '<script type="text/javascript">							
 						Swal.fire({
 							title: "Error!",
-							text: "'.$message.'",
+							text: "' . $message . '",
 							type: "error",
 							timer: 2000,
 							showConfirmButton: false
@@ -47,35 +47,50 @@
 							window.history.back();
 						});
 					</script>';
-			return false;
-		}
-		$stmt->close();
+		return false;
 	}
+	$stmt->close();
+}
+?>
+
+<?php
+$root  = "http://" . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 ?>
 
 <script>
-	function simpanPosisi(posisi){
-		const latitude    = posisi.coords.latitude;
-		const longitude   = posisi.coords.longitude;
-		var id				= '<?php echo $id_sopir;?>';
-		
-		let data = { 'latitude' : latitude, 'longitude' : longitude, 'id' : id };
+	function simpanPosisi(posisi) {
+		const latitude = posisi.coords.latitude;
+		const longitude = posisi.coords.longitude;
+		var id = '<?php echo $id_sopir; ?>';
+
+		let data = {
+			'latitude': latitude,
+			'longitude': longitude,
+			'id': id
+		};
 
 		$.ajax({
 			type: 'POST',
-			url: 'lokasi.php',
+			url: '<?= $root ?>lokasi.php',
 			data: data,
-			// success: function(response){
-			// 	console.log(response);
-			// 	alert('Lokasi berhasil disimpan');
-			// }
+			success: function(response) {
+				console.log(response);
+				// alert('Lokasi berhasil disimpan');
+			},
+			error(xhr, status, eror) {
+				console.log(eror);
+			}
 		});
 	}
-	$(document).ready(function(){
-		navigator.geolocation.getCurrentPosition(simpanPosisi);
+	$(document).ready(function() {
+		setInterval(() => {
+			console.log('update');
+			navigator.geolocation.getCurrentPosition(simpanPosisi);
+		}, 500);
 	});
 </script>
-   
+
 <div class="page-holder w-100 d-flex flex-wrap">
 	<div class="container-fluid px-xl-5">
 		<section class="py-5">
@@ -92,14 +107,14 @@
 									<div class="col-md-3">
 										<select name="id_mobil" class="form-control">
 											<?php
-												$stmt = $con->prepare('SELECT * FROM mobil');
-												$stmt->execute();
-												$stmt->store_result();
-												$stmt->bind_result($id_mobil, $nopol, $merk, $ket_mobil);
-												
-												while ($stmt -> fetch()) {
-													echo '<option value="'.$id_mobil.'" >'.$nopol.'</option>';
-												}
+											$stmt = $con->prepare('SELECT * FROM mobil');
+											$stmt->execute();
+											$stmt->store_result();
+											$stmt->bind_result($id_mobil, $nopol, $merk, $ket_mobil);
+
+											while ($stmt->fetch()) {
+												echo '<option value="' . $id_mobil . '" >' . $nopol . '</option>';
+											}
 											?>
 										</select>
 									</div>
@@ -117,7 +132,7 @@
 			</div>
 		</section>
 	</div>
-	
-<?php
+
+	<?php
 	require_once('footer.php');
-?>
+	?>
