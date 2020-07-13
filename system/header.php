ebe9f341b7d73877efea7fc4f99f7ab4a8a7509a
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-07-13 21:43:08 +0700

    Update header.php

--- system/header.php
+++ system/header.php
@@ -230,7 +230,7 @@ if (!isset($_SESSION['loggedin'])) {
 		</div>
 		<!-- END SIDEBAR -->
 		<?php
-		$root  = "http://" . $_SERVER['HTTP_HOST'];
+		$root  = "https://" . $_SERVER['HTTP_HOST'];
 		$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 		?>
 
