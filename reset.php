1ef84c5448ed25da5ce70a2914c4cfcc6427150d
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-08-08 21:17:59 +0700

    dsa

--- reset.php
+++ reset.php
@@ -53,10 +53,10 @@
 					// if your network does not support SMTP over IPv6
 
 					//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-					$mail->Port = 587;
+					$mail->Port = 465;
 
 					//Set the encryption system to use - ssl (deprecated) or tls
-					$mail->SMTPSecure = 'tls';
+					$mail->SMTPSecure = 'ssl';
 
 					//Whether to use SMTP authentication
 					$mail->SMTPAuth = true;
