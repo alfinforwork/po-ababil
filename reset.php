1d60d4414887b244afeae795601d8fcdb4d61418
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-08-08 21:24:30 +0700

    Update reset.php

--- reset.php
+++ reset.php
@@ -53,7 +53,7 @@
 					// if your network does not support SMTP over IPv6
 
 					//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-					$mail->Port = 587;
+					$mail->Port = 465;
 
 					//Set the encryption system to use - ssl (deprecated) or tls
 					$mail->SMTPSecure = 'tls';
